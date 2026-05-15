c7f78e9ebcf33c9010015a1ca26de3dcee36624f
roronan13 <ronan.pothier@gmail.com>
2026-05-15 18:18:14 +0200

    bon

--- alchemy/__init__.py
+++ alchemy/__init__.py
@@ -1,2 +1,3 @@
 from .elements import create_air
 from .potions import healing_potion as heal, strength_potion
+from .transmutation.recipes import lead_to_gold
--- ft_transmutation_2.py
+++ ft_transmutation_2.py
@@ -4,4 +4,4 @@ import alchemy
 if __name__ == "__main__":
     print(" === Transmutation 2 === ")
 
-    print(f"")
+    print(f"Testing lead to gold : {alchemy.lead_to_gold()}")
