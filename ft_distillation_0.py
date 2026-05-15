d9f3099b574b578f3ec9c9b2e2f5a0a9da749a8f
roronan13 <ronan.pothier@gmail.com>
2026-05-15 17:00:41 +0200

    ok

---
+++ ft_distillation_0.py
@@ -0,0 +1,6 @@
+from alchemy import potions
+
+if __name__ == "__main__":
+    print(" === Distillation 0 === ")
+    print(f"Testing strength_potion : {potions.strength_potion()}")
+    print(f"Testing healing_potion : {potions.healing_potion()}")
---
+++ ft_distillation_1.py
@@ -0,0 +1,6 @@
+import alchemy
+
+if __name__ == "__main__":
+    print(" === Distillation 1 === ")
+    print(f"Testing strength_potion : {alchemy.strength_potion()}")
+    print(f"Testing healing_potion : {alchemy.heal()}")
