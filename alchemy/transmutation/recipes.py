8c3244a698556b26d767cb35cce6b8ba39d5fb5f
roronan13 <ronan.pothier@gmail.com>
2026-05-15 17:57:33 +0200

    oui

---
+++ alchemy/transmutation/recipes.py
@@ -0,0 +1,7 @@
+from ..elements import create_air
+from elements import create_fire
+import alchemy
+
+
+def lead_to_gold() -> str:
+    return (f"Recipe transmutting Lead to Gold : brew '{create_air()}' and '{alchemy.strength_potion()}' mixed with '{create_fire()}'")
---
+++ ft_transmutation_2.py
@@ -0,0 +1,7 @@
+import alchemy
+
+
+if __name__ == "__main__":
+    print(" === Transmutation 2 === ")
+
+    print(f"")
