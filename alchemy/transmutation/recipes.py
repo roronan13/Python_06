4afa8e5d03edc3f917256ea1f281120e4f02bc0a
roronan13 <ronan.pothier@gmail.com>
2026-05-15 18:23:12 +0200

    ok

--- alchemy/transmutation/recipes.py
+++ alchemy/transmutation/recipes.py
@@ -4,4 +4,5 @@ import alchemy
 
 
 def lead_to_gold() -> str:
-    return (f"Recipe transmutting Lead to Gold : brew '{create_air()}' and '{alchemy.strength_potion()}' mixed with '{create_fire()}'")
+    return (f"Recipe transmutting Lead to Gold : brew '{create_air()}' \
+and '{alchemy.strength_potion()}' mixed with '{create_fire()}'")
--- ft_transmutation_0.py
+++ ft_transmutation_0.py
@@ -4,4 +4,5 @@ import alchemy.transmutation.recipes
 if __name__ == "__main__":
     print(" === Transmutation 0 === ")
 
-    print(f"Testing lead to gold : {alchemy.transmutation.recipes.lead_to_gold()}")
+    print(f"Testing lead to gold \
+: {alchemy.transmutation.recipes.lead_to_gold()}")
